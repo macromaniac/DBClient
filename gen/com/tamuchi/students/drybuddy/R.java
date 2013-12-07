0df722e109d482438b36cff381b27f5c32ccbe86
Jerry Barth <lazislacker@gmail.com>
2013-12-07 14:52:02 -0600

    Added contacts.txt

--- gen/com/tamuchi/students/drybuddy/R.java
+++ gen/com/tamuchi/students/drybuddy/R.java
@@ -53,11 +53,18 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int addContactMenu=0x7f050004;
+        public static final int addcontactstr=0x7f05000c;
         public static final int app_name=0x7f050000;
+        public static final int cancelbutt=0x7f05000d;
         public static final int geoloc=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int newcontactbutt=0x7f05000a;
         public static final int numlist=0x7f050006;
         public static final int panic=0x7f050008;
+        public static final int phonenumstr=0x7f05000e;
+        public static final int posscontactstr=0x7f05000b;
+        public static final int refreshbutt=0x7f050009;
+        public static final int sobdatestr=0x7f05000f;
         public static final int title_activity_add_contact=0x7f050003;
         public static final int title_activity_main=0x7f050005;
     }
